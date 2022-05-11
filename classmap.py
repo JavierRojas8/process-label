020dffa5f78f56b70328ae47a1d544c16e015430
JavierRojas8 <100723645+JavierRojas8@users.noreply.github.com>
2022-05-11 01:41:26 -0400

    Add files via upload

---
+++ classmap.py
@@ -0,0 +1,439 @@
+def classmap() :
+    
+    return {
+    0:0,
+    1:1,
+    2:2,
+    3:2,
+    4:3,
+    5:4,
+    6:5,
+    7:1000,
+    8:6,
+        9:7,
+        10:7,
+        11:8,
+        12:9,
+        59:9,
+        13:1000,
+        14:10,
+        15:11,
+        16:12,
+        17:13,
+        18:13,
+        19:14,
+        20:14,
+        21:15,
+        22:16,
+        23:17,
+        24:18,
+        25:19,
+        26:20,
+        27:21,
+        28:22,
+        29:23,
+        79:23,
+        30:24,
+        31:25,
+        33:25,
+        32:26,
+        34:26,
+        35:26,
+        80:26,
+        36:27,
+        37:28,
+        38:29,
+        39:30,
+        40:31,
+        82:31,
+        41:32,
+        42:32,
+        43:32,
+        83:32,
+        44:33,
+        45:34,
+        46:35,
+        47:36,
+        48:37,
+        49:38,
+        50:39,
+        51:39,
+        52:40,
+        53:41,
+        54:42,
+        55:42,
+        56:1000,
+        57:43,
+        58:44,
+        59:44,
+        60:44,
+        61:44,
+        62:44,
+        63:45,
+        64:45,
+        65:46,
+        66:47,
+        67:48,
+        68:49,
+        69:50,
+        70:51,
+        71:51,
+        72:1000,
+        73:52,
+        74:53,
+        75:54,
+        76:55,
+        77:56,
+        78:57,
+        81:58,
+        84:59,
+        85:60,
+        86:60,
+        87:60,
+        88:60,
+        89:1000,
+        90:61,
+        91:62,
+        92:63,
+        93:64,
+        94:65,
+    }
+
+def categories():
+    return [
+        {
+            "id": 0,
+            "name": "fruits",
+            "supercategory": "none"
+        },
+        {
+            "id": 1,
+            "name": "Aji_verde",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 2,
+            "name": "Ajo_bolsa",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 3,
+            "name": "Aloe-vera",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 4,
+            "name": "Apio",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 5,
+            "name": "Brocoli",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 6,
+            "name": "Bruselas",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 7,
+            "name": "Calabaza-amarilla",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 8,
+            "name": "Cebolla",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 9,
+            "name": "Cebolla-pack",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 10,
+            "name": "Champiñon",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 11,
+            "name": "Ciboullette",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 12,
+            "name": "Cilantro",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 13,
+            "name": "Ciruela",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 14,
+            "name": "Cochayuyo",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 15,
+            "name": "Coliflor",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 16,
+            "name": "Curcuma",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 17,
+            "name": "Ensalada",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 18,
+            "name": "Jengibre",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 19,
+            "name": "Kale",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 20,
+            "name": "Kiwano",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 21,
+            "name": "Kiwi",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 22,
+            "name": "Kiwi-bolsa",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 23,
+            "name": "Lechuga",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 24,
+            "name": "Limon",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 25,
+            "name": "Mango",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 26,
+            "name": "Manzana-bolsa",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 27,
+            "name": "Manzana",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 28,
+            "name": "Membrillo",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 29,
+            "name": "Mote",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 30,
+            "name": "Naranja",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 31,
+            "name": "Palta",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 32,
+            "name": "Papa",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 33,
+            "name": "Papa-bolsa",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 34,
+            "name": "Papaya",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 35,
+            "name": "Pepino",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 36,
+            "name": "Pera",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 37,
+            "name": "Pera-Bolsa",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 38,
+            "name": "Pina",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 39,
+            "name": "Pinon",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 40,
+            "name": "Platano",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 41,
+            "name": "Pomelo",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 42,
+            "name": "Repollo",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 43,
+            "name": "Sandia",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 44,
+            "name": "Tangelo",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 45,
+            "name": "Tomate",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 46,
+            "name": "Tomate-caja",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 47,
+            "name": "Tuna",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 48,
+            "name": "Zapallo",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 49,
+            "name": "Zapallo-italiano",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 50,
+            "name": "Zapallo-cubo",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 51,
+            "name": "Arandano-bandeja",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 52,
+            "name": "Choclo-bandeja",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 53,
+            "name": "Esparrago",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 54,
+            "name": "Espinaca",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 55,
+            "name": "Frambuesa-bandeja",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 56,
+            "name": "Frutilla-bandeja",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 57,
+            "name": "Frutilla-picada",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 58,
+            "name": "Hierba",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 59,
+            "name": "Moras",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 60,
+            "name": "Pepino-Dulce",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 61,
+            "name": "Pimenton",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 62,
+            "name": "Pina-pelada",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 63,
+            "name": "Poroto-verde",
+            "supercategory": "fruits"
+        },
+
+        {
+            "id": 64,
+            "name": "Uvas",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 65,
+            "name": "Zanahoria",
+            "supercategory": "fruits"
+        },
+        {
+            "id": 66,
+            "name": "Zanahoria-bolsa",
+            "supercategory": "fruits"
+        }]
